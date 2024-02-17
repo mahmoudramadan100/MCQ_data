3729d3d863a2840f3bd46f8c99ffb07cbc77d508
mahmoud ramadan <mahmoudr03@gmail.com>
2024-02-17 02:16:17 +0200

    add type : module API

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { create, router as _router, defaults } from "json-server"; // importing 
 const server = create();
 const router = _router("db.json");
 const middlewares = defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+const port =  8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
 server.use(router);
